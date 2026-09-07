29dc0df54a3388649d7820d78c342bdb18a335f7
William <wcvno@kth.se>
2026-09-07 09:57:57 +0200

    fix test

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,11 +8,11 @@ class AppTests(unittest.TestCase):
         self.assertIsNone(app.my_function())
 
     def test_overly_complex_early_exit(self):
-        self.assertIsNone(app.overly_complex(False, True, True, True, True))
-        self.assertIsNone(app.overly_complex(True, False, True, True, True))
+        self.assertIsNone(app.overly_complex(False, True, True, True))
+        self.assertIsNone(app.overly_complex(True, False, True, True))
 
     def test_overly_complex_deep_path(self):
-        self.assertIsNone(app.overly_complex(True, True, True, True, True))
+        self.assertIsNone(app.overly_complex(True, True, True, True))
 
     def test_check_db(self):
         self.assertIsNone(app.check_db())
