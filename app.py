fbfe99455009ca6faf4a2a2fc41e45b84e9b55ef
samflo <samflo@kth.se>
2026-09-07 10:16:40 +0200

    check if sonarcloud pass

--- app.py
+++ app.py
@@ -2,19 +2,10 @@
 
 
 def my_function():
-    # unused_variable = 42
     print("korv")
 
 
 # Function with high cyclomatic complexity (maintainability issue)
-def overly_complex(a, b, c, d):
-    if a:
-        if b:
-            if c:
-                if d:
-                    for i in range(10):
-                        if i % 2 == 0 and a or b:
-                            print("Too deep")
 
 
 def check_db():
