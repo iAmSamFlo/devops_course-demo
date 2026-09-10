ec2c73cd642bcce9e7470bc2bdaa8cf8406b12c2
samflo <samflo@kth.se>
2026-09-10 13:36:00 +0200

    return

--- app.py
+++ app.py
@@ -5,14 +5,6 @@ def my_function():
     print("korv")
 
 # Function with high cyclomatic complexity (maintainability issue)
-def overly_complex(a, b, c, d):
-    if a:
-        if b:
-            if c:
-                if d:
-                    for i in range(10):
-                        if i % 2 == 0 and a or b:
-                            print("Too deep")
 
 
 def check_db():
